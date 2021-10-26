ce18803f66b775bc76672a3524fd17fe1c80f2c9
James Ring-Howell <jringhowell@CoralFire.com>
2021-10-26 14:28:35 -0400

    Changes from 10_26_21

---
+++ Phase1Section6.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Newtonsoft.Json.Linq;
+
+namespace Phase1Section6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JArray array = new JArray();
+            JValue text = new JValue("Manual text");
+            JValue date = new JValue(new DateTime(2000, 5, 23));
+
+            array.Add(text);
+            array.Add(date);
+
+            string json = array.ToString();
+            Console.WriteLine(json);
+        }
+
+    }
+}
---
+++ Phase1Section6.9JRH/MyPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Phase1Section6._9JRH
+{
+    public class MyPackage
+    {
+        public void DisplayText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+        }
+
+    }
+}
